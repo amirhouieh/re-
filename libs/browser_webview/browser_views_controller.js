58a1bb356d8acf191abe82d4a42783f70e863299
amirhouieh <amir.houieh@gmail.com>
2016-07-03 16:58:25 +0200

    fix

--- libs/browser_webview/browser_views_controller.js
+++ libs/browser_webview/browser_views_controller.js
@@ -62,8 +62,8 @@ class ViewsController{
             let x = 0;
             while(!isMatch && urlPattern){
                 urlPattern = view.profile.urls[x];
-                // isMatch = uri.href.indexOf(urlPattern) !== -1;
-                isMatch = urlPattern == uri.href;
+                isMatch = uri.href.indexOf(urlPattern) !== -1;
+                // isMatch = urlPattern == uri.href;
                 x++;
             }
 
